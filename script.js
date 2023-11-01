ddf9518aded3160ad480aee46134b2a67c000c9e
Israelvlastuin <124810096+Israelvlastuin@users.noreply.github.com>
2023-10-31 23:13:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function website() {
+    window.open("https://israelvlastuin.github.io/e-comerce/", "_blank"); // Substitua com o link que deseja abrir
+  }
+function github() {
+    window.open("https://github.com/Israelvlastuin", "_blank"); // Substitua com o link que deseja abrir
+  }
+function sobre() {
+    window.open("sobre.html"); // Substitua com o link que deseja abrir
+  }
+function pato(){
+window.open("https://youtu.be/jyKja2vcpQo?si=m-1EYJNzM-5xmxlC")
+}
